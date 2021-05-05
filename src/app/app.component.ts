e22f01dc00a103952d5eb37cc8c0f3e7ba92adf4
unknown <vineetarora010@gmail.com>
2021-05-06 00:41:31 +0530

    Angular-Task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular-Task';
+  public EmpName: string;
+  public EmpNo: number;
+  public  arr = [];
+
+  // tslint:disable-next-line:typedef
+  submitData(){
+    const val = {
+      name: this.EmpName,
+      no: this.EmpNo
+    };
+
+    // this.EmpName = '';
+    // this.EmpNo = null;
+    this.arr.push(val);
+  }
+
+
+  // tslint:disable-next-line:typedef
+  deleteData(i){
+    this.arr.splice(i , 1);
+  }
+
 }
+
